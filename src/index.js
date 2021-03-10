42ecd43f0b10af44645d45ff1c699f9936308f54
Kirill Beliakov <kabeliakov@gmail.com>
2021-03-10 22:45:59 +0300

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) return false;
+  let openBr = [],
+      closeBr = [],
+      array = str.split('');
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openBr.push(bracketsConfig[i][0]);
+    closeBr.push(bracketsConfig[i][1]);
+  }
+  for (let i = array.length - 1; i >= 0; i--) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+        if (array[i] === openBr[j] && array[i + 1] === closeBr[j]) {
+            array.splice(i, 2);
+        }
+    }
+  }
+  return (array.length === 0);
+} 
